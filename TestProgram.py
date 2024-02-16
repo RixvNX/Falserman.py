a8e7ff51b21f9f635bc5ad8197aec2342ba49a1c
RixvNX <142212639+RixvNX@users.noreply.github.com>
2024-02-16 22:54:25 +0800

    Update TestProgram.py
    new examples

--- TestProgram.py
+++ TestProgram.py
@@ -1,4 +1,5 @@
 import Falserman as F
+
 # Exmaple 1  :  Random Spawn English Name (100 times)
 for i in range(1, 101):
     print(f"[{i}]  : {F.Spawner.name(1)[0]}")
@@ -6,3 +7,11 @@ for i in range(1, 101):
 # Exmaple 2  :  Random Spawn Mail (200 times)
 for i in range(1, 201):
     print(f"[{i}]  : {F.Spawner.mail(1)[0]}")
+
+# Exmaple 3  :  Random Spawn Phone Number (100 times)
+for i in range(1, 101):
+    print(f"[{i}]  : {F.Spawner.phonenumber(1, 11)[0]}")
+
+# Exmaple 3  :  Random Spawn Passwords (200 times)
+for i in range(1, 201):
+    print(f"[{i}]  : {F.Spawner.password(1, inSymbol=False)[0]}")
